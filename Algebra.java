8042f3863531ee2cba823653dee00c403ba169b3
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-14 16:45:32 +0200

    Updated Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -13,7 +13,8 @@ public class Algebra {
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(1
+		2,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
@@ -25,43 +26,70 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	while (x2 > 0) {
+		x1++;
+		x2--;
+	}
+	return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+		while (x2 > 0) {
+			x1--;
+			x2--;
+		}
+		return x1;
+		}
+	
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		while (x2 > 0) {
+			x2--;
+			result = plus(result , x1);
+		}
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 1;
+		while (n > 0) {
+			n--;
+			result = times(result , x);
+		}
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int counter = 0;
+		while (x1 >= x2) {
+			x1 = minus(x1 , x2);
+			counter++;
+		}
+		return counter;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int D = div(x1 , x2);
+		int T = times(x2 , D);
+		int M = minus(x1 , T);
+
+		return M;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int r = 0;
+		int R = 0;
+		while (times(r , r) <= x) { 
+			R = r;
+			r++;
+		}
+		return R;
 	}	  	  
 }
