ab588202377c8788d1f760247ba9d95fd4fd6825
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-20 09:36:54 +0200

    Fix div call syntax in Algebra main

--- Algebra.java
+++ Algebra.java
@@ -13,8 +13,7 @@ public class Algebra {
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(1
-		2,3));   // 12 / 3    
+   		System.out.println(div(12,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
